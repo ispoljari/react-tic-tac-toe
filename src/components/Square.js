44816a4877dac62ada16177d8d8523e500c78a02
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-11 16:31:59 +0100

    Rewrite Square CC into FC

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './square.css'
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" 
-      onClick={this.props.onClick}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
+const Square = ({value, onClick}) => (
+  <button className="square" 
+  onClick={onClick}>
+    {value}
+  </button>
+);
 
 export default Square;
