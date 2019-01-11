2ecde1b88e630557f5d1a0d24cc37ff1fd5c7b77
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-11 15:24:47 +0100

    Add state to Square comp & onClick event handler

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './square.css'
 
 class Square extends React.Component {
+  state = {
+    value: null
+  }
+  
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
